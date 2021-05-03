7ce7d8d852a9c8fca3c36b0697d11b759fdd1ed8
Nathan Cooper <nathan.lb.cooper@outlook.com>
2021-05-03 18:16:57 +0200

    Add some text with questions and notes

--- src/UnitOfWorkExample.UnitOfWork/UnitOfWork.cs
+++ src/UnitOfWorkExample.UnitOfWork/UnitOfWork.cs
@@ -11,10 +11,6 @@ namespace UnitOfWorkExample.UnitOfWork
 
     public class UnitOfWork : IUnitOfWork, IDisposable
     {
-        // Note, when using anything other than SQLite I would
-        // 1) I would have passed this class a `string connectionString` and have it open and dispose of the connection, rather than passing it an open connection.
-        // 2) Use async methods
-
         private readonly SQLiteTransaction _transaction;
         public SQLiteConnection Connection { get; }
 
--- test/UnitOfWorkExample.Tests/Examples.cs
+++ test/UnitOfWorkExample.Tests/Examples.cs
@@ -78,6 +78,35 @@ namespace UnitOfWorkExample.Tests
             }
         }
 
+        /**
+         * QUESTIONS
+         *
+         * - The context takes an open connection. What if don't want an open connection for the lifetime of my request, application etc?
+         *      -> Usually I would have UnitOfWork take a `string connectionString` and have it manage the connection.
+         *          I haven't here because in these SQLite tests the Database only exists as long as the connection is open
+         *
+         * - Wouldn't async methods be better?
+         *      -> Yes. Just not for SQLite.
+         *
+         * - What if I had, for example, an EntityCreated event I needed to publish only if I make changes and Commit them. How would I make that work with the UnitOfWork?
+         *      -> Either use or create some kind of transactional event publisher
+         *              At its most basic, it needs a method to collect up the events you want to send and it needs a Commit() method that actually sends the events.
+         *              Call the commit method from the UnitOfWork Commit()
+         *          You can put anything you want in your UOW really. Multiple databases, event publishing, hangfire scheduling etc. Whatever you think belongs together conceptually as a unit of work.
+         *
+         * - Can I have an example of how I would add this to my IOC container?
+         *      -> Yes, for SimpleInjector:
+         *              var uowRegistration = Lifestyle.Scoped.CreateRegistration<UnitOfWorkContext>(
+         *              () => new UnitOfWorkContext(container.GetInstance<SqlSettings>().ConnectionString), container);
+         *              container.AddRegistration(typeof(ICreateUnitOfWork), uowRegistration);
+         *              container.AddRegistration(typeof(IGetUnitOfWork), uowRegistration);
+         *              container.Register<EntityRepository>(Lifestyle.Scoped);
+         *
+         * - What if I have multiple databases with multiple different UnitOfWork I want to be distinct and manage separately. How do I do that without duplicating this code?
+         *      -> I've tried just making everything pointlessly generic. eg IUnitOfWork<TConnection>, where TConnection is an empty type named after the database.
+         *          That way I can't mix up different UOWs or UOWContexts because the compiler won't let me.
+         */
+
         private static SQLiteConnection SetupDatabase()
         {
             var connection = new SQLiteConnection("Data Source=:memory:");
